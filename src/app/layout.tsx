01f53e0583c7f338dbcbd0729ef6d56462a99e1a
irinakadushkina <kadushkina.i@list.ru>
2024-06-07 20:18:14 +0300

    feat: connect material ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import StyledComponentsRegistry from './registry'
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,8 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+
       <body className={inter.className}>
-        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+          <AppRouterCacheProvider>
+            <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+          </AppRouterCacheProvider>
       </body>
     </html>
   );
