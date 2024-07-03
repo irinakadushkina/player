2ce14a184cae462f469dd9193be8f088d70d4865
irinakadushkina <kadushkina.i@list.ru>
2024-07-03 21:06:42 +0300

    refactor: routes as array

--- src/app/common-components/navbar.tsx
+++ src/app/common-components/navbar.tsx
@@ -2,6 +2,28 @@ import { Theme, useTheme } from "@mui/material";
 import Link from "next/link"
 import styled from "styled-components";
 
+
+const ROUTES = [
+    { 
+        id: 'dashboard',
+        name: 'Dashboard',
+        icon: null,
+        url: '/dashboard'
+    },
+    { 
+        id: 'my',
+        name: 'My playlists',
+        icon: null,
+        url: '/playlists'
+    },
+    { 
+        id: 'podcasts',
+        name: 'Podcasts',
+        icon: null,
+        url: '/podcasts'
+    },
+]
+
 const NavContainer = styled.nav`
     display: flex;
     flex-direction: column;
@@ -23,7 +45,7 @@ const NavLink = styled(Link)<{ theme: Theme }>`
     transition: .25s;
 
     &:hover {
-        background: ${({ theme }) => theme.palette.primary.light};
+        background: ${({ theme }) => `${theme.palette.primary.main}50`};
         color: ${({ theme }) => theme.palette.primary.dark};
     }
 `;
@@ -33,8 +55,9 @@ export const NavBar = () => {
 
     return (
         <NavContainer>
-            <NavLink href="/dashboard" theme={theme}>Dashboard</NavLink>
-            <NavLink href="/podcasts" theme={theme}>Podcasts</NavLink>
+            {
+                ROUTES.map(({id, name, url})=> <NavLink key={id} href={url} theme={theme}>{name}</NavLink>)
+            }
         </NavContainer>
     )
 }
