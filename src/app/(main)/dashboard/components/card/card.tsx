f958d4eacea73cc1678ff8a89c8279d134616a1a
irinakadushkina <kadushkina.i@list.ru>
2024-10-04 22:16:17 +0300

    test: add next image

--- src/app/(main)/dashboard/components/card/card.tsx
+++ src/app/(main)/dashboard/components/card/card.tsx
@@ -1,12 +1,14 @@
 'use client'
 import { Box, useTheme } from "@mui/material";
+import Image from 'next/image'
+import test from '../../../../../../public/assets/mock-cover-recomends.png';
 
 const Card = () => {
     const theme = useTheme();
 
     return (
-        <Box sx={{ border: `1px solid ${theme.palette.primary.dark}`, borderRadius: '15px', padding: '8px', height: '150px', width: '300px', flex: 'none' }}>
-            test
+        <Box sx={{ border: `1px solid ${theme.palette.primary.dark}`, borderRadius: '15px', height: '150px', width: '300px', flex: 'none', overflow: 'hidden' }}>
+            <Image src={test} height={150} alt='test' />
         </Box>
     );
 };
