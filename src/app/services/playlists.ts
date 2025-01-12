bc59df642e70b00899a1705ce40d0a03f0b41965
irinakadushkina <kadushkina.i@list.ru>
2025-01-12 12:23:24 +0300

    fix(api): no-cache

--- src/app/services/playlists.ts
+++ src/app/services/playlists.ts
@@ -2,7 +2,7 @@ import { getBaseUrl } from "../helpers/get-base-url";
 import { PlaylistType } from "../types/playlists";
 
 export const getPlaylists = async () => {
-    const res = await fetch(getBaseUrl() + '/api/playlists', { method: 'GET' });
+    const res = await fetch(getBaseUrl() + '/api/playlists', { method: 'GET', cache:"no-cache" });
     try {
         const { data } = await res.json();
     return data;
@@ -12,7 +12,7 @@ export const getPlaylists = async () => {
 };
 
 export const getPlaylist = async (id: string): Promise<PlaylistType | undefined> => {
-    const res = await fetch(getBaseUrl() + `/api/playlists/${id}`, { method: 'GET' });
+    const res = await fetch(getBaseUrl() + `/api/playlists/${id}`, { method: 'GET', cache:"no-cache" });
     try {
         const { data } = await res.json();
     return data;
