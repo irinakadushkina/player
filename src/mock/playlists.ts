72927eefc6b86421621a6766063e709a4af40ca5
irinakadushkina <kadushkina.i@list.ru>
2025-01-12 12:15:24 +0300

    feat(mock): add test playlist

--- src/mock/playlists.ts
+++ src/mock/playlists.ts
@@ -5,10 +5,10 @@ export const playlists = [
         cover: 'https://avatars.yandex.net/get-music-content/6300975/13e0cdd0.a.21992206-1/200x200',
         tracks: [ '000', '001', '002', '003' ],
     },
-    // {
-    //     id: '001',
-    //     title: 'test playlist 2',
-    //     cover: '',
-    //     tracks: [ '002', '003' ],
-    // }
+    {
+        id: '001',
+        title: 'test playlist 2',
+        cover: 'https://avatars.yandex.net/get-music-content/99892/d255c320.a.7284080-1/200x200',
+        tracks: [ '003' ],
+    }
 ];
