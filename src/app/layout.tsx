e999fafffea55597a5f58648f3e62dc28517db78
irinakadushkina <kadushkina.i@list.ru>
2025-04-14 17:32:08 +0300

    feat(style): add font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,9 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
       </head>
       <body className={inter.className}>
           <ThemeProvider theme={darkTheme}>
